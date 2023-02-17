1785ecf436f435f1e28ad50a9981d7c0c167bf18
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-02-17 16:24:42 +0700

    update lab1 orginal program base on improved version

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,93 +1,80 @@
-#import json
+students = []
+courses = []
+marks = {}
 
 
-def input_student(lst):
-    n = int(input("Enter number of students: "))
-    for i in range(n):
-        student_id = str(input("Enter student id: "))
-        student_name = str(input("Enter student name: "))
-        dob = str(input("Enter dob: "))
-        student.append({"id": student_id, "name": student_name, "dob": dob})
+#input student
+def input_student():
+    print("Enter student details:")
+    student_id = str(input("Student ID: "))
+    student_name = str(input("Student name: "))
+    dob = str(input("Date of birth (DD-MM-YYYY): "))
+    students.append({"student_id": student_id, "student_name": student_name, "dob": dob})
+    print(f"Student {student_id} {student_name} added.")
 
 
-def input_course(lst):
-    n = int(input("Enter number of Courses: "))
-    for i in range(n):
-        course_id = str(input("Enter Course id: "))
-        course_name = str(input("Enter Course name: "))
-        course.append({"id": course_id, "name": course_name})
+#input course
+def input_course():
+    print("Enter course details:")
+    course_id = str(input("Course ID: "))
+    course_name = str(input("Course name: "))
+    courses.append({"course_id": course_id, "course_name": course_name})
+    marks[course_id] = {}
+    for student in students:
+        marks[course_id][student["student_id"]] = "N/A"
+    print(f"Course {course_id} {course_name} added.")
 
 
-def input_mark(lst):
-    n = str(input("Enter Course name or id: "))
-    student_id = str(input("Enter student id: "))
-    for i in range(len(course)):
-        if n == course[i]["id"] or n == course[i]["name"]:
-            print("Course: ", course[i]["id"], course[i]["name"])
-            for j in range(len(student)):
-                if student_id == student[j]["id"]:
-                    mark = float(input("Enter mark for student: " + student[j]["id"] + " " + student[j]["name"] + ": "))
-                    student[j][course[i]["id"]] = mark
-            for j in range(len(student)):
-                #check if student has mark for course
-                if course[i]["id"] not in student[j]:
-                    student[j][course[i]["id"]] = "No mark"
+#input mark
+def input_mark():
+    print("Enter mark details:")
+    course_id = input("Course ID: ")
+    student_id = input("Student ID: ")
+    mark = float(input("Mark: "))
+    marks[course_id][student_id] = mark
+    print(f"Mark added for student ID {student_id} in course ID {course_id}.")
 
 
-def table_out(lst):
-    print("Student_id".ljust(15), "Student_name".ljust(15), "Student_DoB".ljust(15), end="")
-    for i in range(len(course)):
-        print(course[i]["id"].ljust(15), end="")
-    print()
-    for i in range(len(student)):
-        print(student[i]["id"].ljust(15), student[i]["name"].ljust(15), student[i]["dob"].ljust(15), end="")
-        for j in range(len(course)):
-            print(str(student[i][course[j]["id"]]).ljust(15), end="")
-        print()
+#output report
+def output_report(): #this function is not totally mine because I'm bad at handling visual stuff
+    print("\nChoose a course:")
+    for course in courses:
+        print(f"{course['course_id']}. {course['course_name']}")
+    course_id = str(input("Option: "))
+    print(f"\nReport for course :")
 
+    #show course info
+    for course in courses:
+        if course_id == course["course_id"]:
+            print(f"{course['course_id']}. {course['course_name']}")
 
-def student_info_lst(lst):
-    print("\n\n")
-    print("Student_id".ljust(15), "Student_name".ljust(15), "Student_DoB".ljust(15))
-    for i in range(len(student)):
-        print(student[i]["id"].ljust(15), student[i]["name"].ljust(15), student[i]["dob"].ljust(15))
-
-def course_info_lst(lst):
-    print("\n\n")
-    print("Course_id".ljust(15), "Course_name".ljust(15))
-    for i in range(len(course)):
-        print(course[i]["id"].ljust(15), course[i]["name"].ljust(15))
-
-
-#def json_out(lst):
-#    with open("student.json", "w") as f:
-#        json.dump(lst, f, indent=4)
-
+    print("{:<10} {:<20} {:<15} {:<10}".format("ID", "Name", "Date of Birth", "Mark"))
+    for student in students:
+        mark = marks[course_id][student['student_id']]
+        print("{:<10} {:<20} {:<15} {:<10}".format(student['student_id'], student['student_name'], student['dob'], mark))
 
 if __name__ == "__main__":
-    student = []
-    course = []
-    sw = True
-    while sw:
-        print("1. Input student \n2. Input course \n3. Input mark \n4. Output \n5. Exit")
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
-            input_student(student)
-        elif choice == 2:
-            input_course(course)
-        elif choice == 3:
-            input_mark(course)
-        elif choice == 4:
-            course_info_lst(course)
-            student_info_lst(student)
-            table_out(student)
-            #print(f"\n\n{student}")
-        elif choice == 5:
-            sw = False
+    while True:
+        print("\nChoose an option:")
+        print("1. Add student")
+        print("2. Add course")
+        print("3. Add mark")
+        print("4. Print report")
+        print("5. Exit")
+        option = int(input("Option: "))
+
+        if option == 1:
+            n = int(input("Number of students: "))
+            for i in range(0,n):
+                input_student()
+        elif option == 2:
+            input_course()
+        elif option == 3:
+            input_mark()
+        elif option == 4:
+            output_report()
+        elif option == 5:
+            break
         else:
-            print("Invalid choice")
-
+            print("Invalid option. Try again.")
     exit()
-
-    # output json file
-    #json_out(student)
