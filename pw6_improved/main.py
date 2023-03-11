7fe2a851efeefc724ba4bd483bce9385b3bf11f3
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-03-11 09:22:26 +0700

    add error catching in main

--- pw6_improved/main.py
+++ pw6_improved/main.py
@@ -2,4 +2,11 @@ from menu import Menu
 import curses
 if __name__ == "__main__":
     menu = Menu()
-    menu.main()
+    try:
+        curses.wrapper(menu.main())
+    except Exception as e:
+        # Display the error message in a separate window
+        stdscr = curses.initscr()
+        stdscr.addstr(0, 0, str(e))
+        stdscr.getch()
+        curses.endwin()
