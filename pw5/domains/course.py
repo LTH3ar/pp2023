52a002be0819f21b3a53ca6579dbfced9e6c1c1d
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-03-14 13:48:51 +0700

    remove old pw5 & 6

--- pw5/domains/course.py
+++
@@ -1,9 +0,0 @@
-class Course:
-    def __init__(self, course_id, course_name, course_credit):
-        self.id = course_id
-        self.name = course_name
-        self.credit = course_credit
-        self.marks = {}
-
-    def input_mark(self, student_id, mark):
-        self.marks[student_id] = mark
--- pw5/main.py
+++
@@ -1,5 +0,0 @@
-from menu import Menu
-import curses
-if __name__ == "__main__":
-    menu = Menu()
-    menu.main()
