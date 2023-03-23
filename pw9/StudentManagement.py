a297f05d1a012185fca1e3ad30797753e793304c
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-03-23 15:22:32 +0700

    fix GPA ranking function

--- pw9/StudentManagement.py
+++ pw9/StudentManagement.py
@@ -12,17 +12,17 @@ from domains.student import Student
 class StudentManagement:
     #init
     def __init__(self, menu_window):
-        self.student_list = []
-        self.course_list = []
-        self.mark_list = []
+        self.__student_list = []
+        self.__course_list = []
+        self.__mark_list = []
         self.menu_window = menu_window
-        self.input_funcs = Input(self.student_list,
-                                 self.course_list,
-                                 self.mark_list,
+        self.input_funcs = Input(self.__student_list,
+                                 self.__course_list,
+                                 self.__mark_list,
                                  self.menu_window)
-        self.output_funcs = Output(self.student_list,
-                                   self.course_list,
-                                   self.mark_list,
+        self.output_funcs = Output(self.__student_list,
+                                   self.__course_list,
+                                   self.__mark_list,
                                    self.menu_window)
         self.background_thread()
 
@@ -33,13 +33,13 @@ class StudentManagement:
     def gpa_calculator(self):
         max_point = float(20)
         max_credit = 0
-        for credit in self.course_list:
+        for credit in self.__course_list:
             max_credit += int(credit.get_credit())
-        for student in self.student_list:
+        for student in self.__student_list:
             gpa_sum = 0
             credits_sum = 0
-            for course in self.course_list:
-                for mark in self.mark_list:
+            for course in self.__course_list:
+                for mark in self.__mark_list:
                     if (course.get_id() == mark.get_course_id()
                             and mark.get_student_id() == student.get_id()):
                         mark_mid = ((float(mark.get_mark_mid()) / max_point)
@@ -74,12 +74,15 @@ class StudentManagement:
             ('id', np.str_, 16),
             ('name', np.str_, 16),
             ('dob', np.str_, 10),
-            ('__gpa', np.float32)
+            ('gpa', np.float32)
         ])
 
         tmp_gpa = []
-        for student in self.student_list:
-            tmp_gpa.append(student)
+        for student in self.__student_list:
+            tmp_gpa.append(Student(student.get_id(),
+                                   student.get_name(),
+                                   student.get_dob(),
+                                   student.get_gpa()))
 
         for i in tmp_gpa:
             if i.get_gpa() == "N/A":
@@ -90,29 +93,32 @@ class StudentManagement:
         # Convert the list to a structured numpy array
         gpa_arr = np.array(gpa_list, dtype=student_dtype)
         # Sort the array by GPA
-        gpa_arr = np.sort(gpa_arr, order='__gpa')
+        gpa_arr = np.sort(gpa_arr, order='gpa')
         # re convert to list of class Student
-        gpa_list = []
+        tmp_gpa.clear()
         for s in gpa_arr:
             student_id = s[0]
             student_name = s[1]
             student_dob = s[2]
             student_gpa = s[3]
-            gpa_list.append(Student(student_id, student_name, student_dob, student_gpa))
+            tmp_gpa.append(Student(student_id, student_name, student_dob, student_gpa))
 
-        self.output_funcs.output_students_list_sorted(gpa_list)
+        self.output_funcs.output_students_list_sorted(tmp_gpa)
 
     def gpa_ranking_High2Low(self):
         student_dtype = np.dtype([
             ('id', np.str_, 16),
             ('name', np.str_, 16),
             ('dob', np.str_, 10),
-            ('__gpa', np.float32)
+            ('gpa', np.float32)
         ])
 
         tmp_gpa = []
-        for student in self.student_list:
-            tmp_gpa.append(student)
+        for student in self.__student_list:
+            tmp_gpa.append(Student(student.get_id(),
+                                   student.get_name(),
+                                   student.get_dob(),
+                                   student.get_gpa()))
 
         for i in tmp_gpa:
             if i.get_gpa() == "N/A":
@@ -123,17 +129,17 @@ class StudentManagement:
         # Convert the list to a structured numpy array
         gpa_arr = np.array(gpa_list, dtype=student_dtype)
         # Sort the array by GPA
-        gpa_arr = np.sort(gpa_arr, order='__gpa')[::-1]
+        gpa_arr = np.sort(gpa_arr, order='gpa')[::-1]
         # re convert to list of class Student
-        gpa_list = []
+        tmp_gpa.clear()
         for s in gpa_arr:
             student_id = s[0]
             student_name = s[1]
             student_dob = s[2]
             student_gpa = s[3]
-            gpa_list.append(Student(student_id, student_name, student_dob, student_gpa))
+            tmp_gpa.append(Student(student_id, student_name, student_dob, student_gpa))
 
-        self.output_funcs.output_students_list_sorted(gpa_list)
+        self.output_funcs.output_students_list_sorted(tmp_gpa)
 
     def option_select(self, input_option):
 
