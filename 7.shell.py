af5f202d6931af1c15ef4be7032e784c20a26dba
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-03-28 17:34:27 +0700

    rewrite shell lab7

--- 7.shell.py
+++ 7.shell.py
@@ -1,125 +1,191 @@
-import os
 import subprocess
+import os
 
-class PythonShell:
+class command_struct:
+    def __init__(self, command_string):
+        self.command_string = command_string.split()
+        self.command = self.command_string[0]
+        self.args = self.command_string[1:]
+
+
+class UserInput:
     def __init__(self):
-        self.current_dir = os.getcwd()
+        self.input = None
 
-    def run_command(self, command):
-        # split the command string into a list of arguments
-        args = command.split()
-
-        # check for IO redirection and pipe operators
-        input_redirect = None
-        output_redirect = None
-        append_redirect = None
-        pipe_operator = None
-        for i, arg in enumerate(args):
-            if arg == "<":
-                input_redirect = args[i+1]
-                args.pop(i)
-                args.pop(i)
-                break
-            elif arg == ">":
-                output_redirect = args[i+1]
-                args.pop(i)
-                args.pop(i)
-                break
-            elif arg == ">>":
-                append_redirect = args[i+1]
-                args.pop(i)
-                args.pop(i)
-                break
-            elif arg == "|":
-                pipe_operator = i
-                break
-
-        if pipe_operator:
-            # handle piped commands
-            command1 = " ".join(args[:pipe_operator])
-            command2 = " ".join(args[pipe_operator+1:])
-            process1 = subprocess.Popen(command1, 
-                                        stdout=subprocess.PIPE, 
-                                        shell=True, 
-                                        cwd=self.current_dir)
-            process2 = subprocess.Popen(command2, 
-                                        stdin=process1.stdout,
-                                        stdout=subprocess.PIPE, 
-                                        shell=True, 
-                                        cwd=self.current_dir)
-            process1.stdout.close()
-            output = process2.communicate()[0]
-            if output_redirect:
-                with open(output_redirect, "w") as output_file:
-                    output_file.write(output.decode())
-            elif append_redirect:
-                with open(append_redirect, "a") as append_file:
-                    append_file.write(output.decode())
-            else:
-                print(output.decode())
+    def get_input(self):
+        self.input = input("myshell> ")
+        if self.input == "":
+            return
         else:
-            # execute the command with subprocess.Popen()
-            process = subprocess.Popen(args,
-                                       stdin=subprocess.PIPE,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       cwd=self.current_dir)
+            self.input = self.input.split("|")
+            command_count = len(self.input)
+            for i in range(command_count):
+                self.input[i] = command_struct(self.input[i])
 
-            # handle input redirection
-            if input_redirect:
-                with open(input_redirect, "r") as input_file:
-                    input_data = input_file.read()
-                    process.stdin.write(input_data.encode())
-                    process.stdin.close()
+    def print_input(self):
+        for i in self.input:
+            print(i.command + " " + str(i.args))
 
-            # handle output redirection
-            if output_redirect:
-                with open(output_redirect, "w") as output_file:
-                    output = process.stdout.read()
-                    output_file.write(output.decode())
-            elif append_redirect:
-                with open(append_redirect, "a") as append_file:
-                    output = process.stdout.read()
-                    append_file.write(output.decode())
+
+class Shell:
+    def __init__(self):
+        self.user_input = UserInput()
+
+    def run(self):
+        while True:
+            self.user_input.get_input()
+            self.user_input.print_input()
+            self.run_command_check()
+
+    def change_dir(self):
+        if self.user_input.input[0].args[0] == '~':
+            os.chdir(os.path.expanduser("~"))
+        else:
+            os.chdir(self.user_input.input[0].args[0])
+
+    def run_command_check(self):
+        if len(self.user_input.input) == 0:
+            return
+
+        if self.user_input.input[0].command == "cd":
+            self.change_dir()
+        elif self.user_input.input[0].command == "exit":
+            exit()
+        else:
+            if len(self.user_input.input) == 1:
+                self.execute_command_single()
             else:
-                # print the output to the console
-                output = process.stdout.read()
-                print(output.decode())
+                self.execute_command_pipe()
 
-            # close the file descriptors and wait for the process to terminate
-            process.stdin.close()
-            process.stdout.close()
-            process.stderr.close()
-            process.wait()
+    def io_redirection(self, command_raw):
+        output_file = None
+        input_file = None
+        sign_io = [">", "<", ">>"]
 
-    def change_dir(self, path):
-        os.chdir(path)
-        self.current_dir = os.getcwd()
+        # if command_raw not in sign_io:
+        #    return command_raw, None, None
 
-    def get_current_dir(self):
-        return self.current_dir
+        for token in command_raw:
+
+            if token == "<":
+                input_file = command_raw[command_raw.index(token) + 1]
+                command_raw.remove(token)
+                command_raw.remove(input_file)
+                # print(command_raw)
+
+        for token in command_raw:
+            if token == ">":
+                output_file = command_raw[command_raw.index(token) + 1]
+                command_raw.remove(token)
+                command_raw.remove(output_file)
+                # print(command_raw)
+
+            elif token == ">>":
+                output_file = command_raw[command_raw.index(token) + 1]
+                command_raw.remove(token)
+                command_raw.remove(output_file)
+                # print(command_raw)
+
+        # print(command_raw)
+
+        return command_raw, input_file, output_file
+
+    def execute_command_single(self):
+        command, input_file, output_file = self.io_redirection(self.user_input.input[0].command_string)
+        # print(command, input_file, output_file)
+
+        if input_file and not output_file:
+            with open(input_file, "r") as input_file:
+                input_file = input_file
+                exec_command = subprocess.Popen(command, stdin=input_file, stdout=subprocess.PIPE, shell=False)
+
+        elif output_file and not input_file:
+            with open(output_file, "w") as output_file:
+                output_file = output_file
+                exec_command = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=output_file, shell=False)
+
+        elif input_file and output_file:
+            with open(input_file, "r") as input_file:
+                input_file = input_file
+                with open(output_file, "w") as output_file:
+                    exec_command = subprocess.Popen(command, stdin=input_file, stdout=output_file, shell=False)
+                    output_file = output_file
+        else:
+            exec_command = subprocess.Popen(command, shell=False)
+
+        exec_command.wait()
+        output, error = exec_command.communicate()
+        if output:
+            output_lst = (output.decode().split("\n"))
+            for i in output_lst:
+                print(i)
+        if error:
+            print(error.decode())
+
+    def execute_command_pipe(self):
+        command_lst = []
+        input_file_lst = []
+        output_file_lst = []
+        for i in range(len(self.user_input.input)):
+            command, input_file, output_file = self.io_redirection(self.user_input.input[i].command_string)
+            input_file_lst.append(input_file)
+            output_file_lst.append(output_file)
+            command_raw = command_struct(" ".join(command))
+            command_lst.append(command_raw)
+        if any(input_file_lst) and not any(output_file_lst):
+            with open(input_file_lst[0], "r") as input_file:
+                input_file = input_file
+                exec_command = subprocess.Popen(command_lst[0].command_string, stdin=input_file, stdout=subprocess.PIPE,
+                                                shell=False)
+                for i in range(1, len(self.user_input.input)):
+                    exec_command = subprocess.Popen(command_lst[i].command_string, stdin=exec_command.stdout,
+                                                    stdout=subprocess.PIPE, shell=False)
+
+        elif any(output_file_lst) and not any(input_file_lst):
+            for i in output_file_lst:
+                if i != None:
+                    with open(i, "w") as output_file:
+                        output_file = output_file
+                        exec_command = subprocess.Popen(command_lst[0].command_string, stdin=subprocess.PIPE,
+                                                        stdout=subprocess.PIPE, shell=False)
+                        for i in range(1, len(self.user_input.input)):
+                            exec_command = subprocess.Popen(command_lst[i].command_string, stdin=exec_command.stdout,
+                                                            stdout=output_file, shell=False)
+
+        elif any(input_file_lst) and any(output_file_lst):
+            with open(input_file_lst[0], "r") as input_file:
+                input_file = input_file
+                for i in output_file_lst:
+                    if i != None:
+                        with open(i, "w") as output_file:
+                            output_file = output_file
+                            exec_command = subprocess.Popen(command_lst[0].command_string, stdin=input_file,
+                                                            stdout=subprocess.PIPE, shell=False)
+                            for i in range(1, len(self.user_input.input)):
+                                exec_command = subprocess.Popen(command_lst[i].command_string,
+                                                                stdin=exec_command.stdout, stdout=output_file,
+                                                                shell=False)
+        else:
+            exec_command = subprocess.Popen(self.user_input.input[0].command_string, stdout=subprocess.PIPE,
+                                            shell=False)
+            for i in range(1, len(self.user_input.input)):
+                exec_command = subprocess.Popen(self.user_input.input[i].command_string, stdin=exec_command.stdout,
+                                                stdout=subprocess.PIPE, shell=False)
+
+        exec_command.wait()
+
+        output, error = exec_command.communicate()
+        # exec_command.stdout.close()
+
+        if output:
+            output_lst = (output.decode().split("\n"))
+            for i in output_lst:
+                print(i)
+
+        if error:
+            print(error.decode())
+
 
 if __name__ == "__main__":
-    shell = PythonShell()
-    while True:
-        user_input = input(f"{shell.get_current_dir()} $ ")
-
-        if user_input == "":
-            continue
-
-        if user_input == "clear":
-            os.system("clear")
-            continue
-
-        if user_input == "exit":
-            break
-
-        if user_input.startswith("cd"):
-            path = user_input.split()[1]
-            if path == "~":
-                path = os.path.expanduser("~")
-            shell.change_dir(path)
-            continue
-        else:
-            shell.run_command(user_input)
-
+    shell = Shell()
+    shell.run()
